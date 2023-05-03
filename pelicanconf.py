42f15b8020407759f95c9ea6007908b9c4850190
dunossauro <mendesxeduardo@gmail.com>
2023-05-03 17:40:57 -0300

    Dump do medium

---
+++ pelicanconf.py
@@ -0,0 +1,22 @@
+AUTHOR = '@dunossauro'
+SITENAME = 'Blog do Dunossauro'
+SITEURL = ''
+THEME = 'notmyidea' # notmyidea
+
+PATH = 'content'
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = 'pt'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Página principal', 'https://dunossauro.com'),)
+
+DEFAULT_PAGINATION = 5
